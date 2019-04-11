fccfcf219401fe55633ddf6a83c7fb05cb1a1a95
meucham11 <34879309+meucham11@users.noreply.github.com>
2019-04-11 16:22:43 +0900

    드디어 로그인 성공

--- 연습코드.R
+++ 연습코드.R
@@ -9,51 +9,25 @@ library(stringr)
 library(plyr)
 library(dplyr)
 
-### 로그인
-login="https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fnote.naver.com"
-pgsession<-html_session(login)
-(pgform=html_form(pgsession))
+####################
+install.packages("RSelenium")
+library(RSelenium)
 
-filled_form=set_values(pgform[[1]], id="meucham", pw="zbqmWjd11!@#")
+remDr <- remoteDriver(remoteServerAddr = 'localhost', 
+                      port = 4445L, # 포트번호 입력 
+                      browserName = "chrome") 
+remDr$open() 
+remDr$navigate("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com") 
+html <- remDr$getPageSource()[[1]] 
+html <- read_html(html) 
 
+id <- remDr$findElement(using="xpath", value='//*[@id="id"]') 
+pw <- remDr$findElement(using="xpath", value='//*[@id=\"pw\"]') 
 
-#제출
-a=submit_form(pgsession, filled_form)
-
-b=jump_to(a,note)
-read_html(b)
-
-note='https://note.naver.com/'
-read_html(note) %>% print(500)
-
-
-
-#######팬텀 js
-setwd('D:\\다운로드')
-phantomjs = 'D:\\다운로드\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin'
-scrape = 'D:\\다운로드\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\naver.js'
-exec_scrape = paste0(phantomjs, scrape)
-
-system(exec_scrape)
-
-batches <- read_html("techstars.html") %>%
-  html_nodes(".batch")
-
-class(batches)
-
-batch_titles <- batches %>%
-  html_nodes(".batch_class") %>%
-  html_text()
+id$sendKeysToElement(list("")) 
+pw$sendKeysToElement(list("")) 
 
 
-
-#######팬텀 js2
-setwd("D:\\다운로드\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin")
-system("phantomjs --version")
-system("testcas1.js")
-system("phantomjs ./test.js")
-
-read_html(a)
-
-
-
+btn <- remDr$findElement(using="xpath", value='//*[@id="frmNIDLogin"]/fieldset/input') 
+# 로그인 버튼 클릭 
+btn$clickElement() 
