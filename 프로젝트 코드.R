3dc3c5e3f830c27c887ec00aa1bf7429374e39c2
meucham11 <34879309+meucham11@users.noreply.github.com>
2019-04-12 17:52:17 +0900

    팝업닫기까지 성공

--- 프로젝트 코드.R
+++ 프로젝트 코드.R
@@ -1,4 +1,5 @@
-# 아이템매니아
+네이버 로그인후 쪽지
+
 
 library(rvest)
 library(selectr)
@@ -7,26 +8,41 @@ library(XML)
 library(stringr)
 library(plyr)
 library(dplyr)
-
-####################
-install.packages("RSelenium")
 library(RSelenium)
+####################
+
 
 remDr <- remoteDriver(remoteServerAddr = 'localhost', 
                       port = 4445L, # 포트번호 입력 
                       browserName = "chrome") 
 remDr$open() 
-remDr$navigate("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com") 
-html <- remDr$getPageSource()[[1]] 
-html <- read_html(html) 
-
-id <- remDr$findElement(using="xpath", value='//*[@id="id"]') 
-pw <- remDr$findElement(using="xpath", value='//*[@id=\"pw\"]') 
-
-id$sendKeysToElement(list("아이디")) 
-pw$sendKeysToElement(list("비밀번호")) 
+remDr$navigate("http://www.itemmania.com/portal/user/p_login_form.html?returnUrl=http%3A%2F%2Fwww.itemmania.com%2Fmyroom%2F")
 
 
-btn <- remDr$findElement(using="xpath", value='//*[@id="frmNIDLogin"]/fieldset/input') 
+id <- remDr$findElement(using="xpath", value='//*[@id="user_id"]') 
+pw <- remDr$findElement(using="xpath", value='//*[@id=\"user_password\"]') 
+
+id$sendKeysToElement(list("meucham1")) 
+pw$sendKeysToElement(list("zbqmWkd11!@#")) 
+
 # 로그인 버튼 클릭 
-btn$clickElement() 
+login_btn=remDr$findElement(using="xpath",value='//*[@id="login_before"]/div[1]/img')
+login_btn$clickElement()  #로그인 성공 후 마이룸으로
+
+#팝니다 원하는 게임, 서버 접속
+game_box <- remDr$findElement(using="xpath", value='//*[@id="searchGameServer"]')
+game_box$sendKeysToElement(list("바람의나라")) 
+game_select=remDr$findElement(using="xpath",value='//*[@id="g_gameServer"]/div[1]/ul/li[1]/span')
+game_select$clickElement()
+server_select=remDr$findElement(using="xpath",value='//*[@id="g_gameServer"]/div[2]/ul/li[2]')
+server_select$clickElement()
+
+
+#팝업창 지우기
+pop_up2=remDr$findElement(using="xpath",value='//*[@id="listSearchDjb_inptDeny"]')
+pop_up2$clickElement()
+pop_up2=remDr$findElement(using="xpath",value='//*[@id="listSearchDjb"]/div[1]/map/area[1]')
+pop_up2$clickElement()
+
+
+#아이템류 선택 후 아이템 검색
